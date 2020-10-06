0288044586ca05212346167d3574a52dd6462f4a
Sergio <sergioobolevich@gmail.com>
2020-10-06 17:41:12 -0300

    Add my_all method

--- enumerables.rb
+++ enumerables.rb
@@ -31,8 +31,35 @@ module Enumerable
   end
 
   # my_all Method
-
+  def my_all?(type = nil)
+    if block_given? and type == nil
+      flag = true
+      self.my_each do |n|
+        if n.is_a?(String)
+          flag = false if not yield(n)
+        elsif n.is_a?(Numeric)
+          flag = false if not yield(n)
+        end
+      end
+      flag
+    elsif type.is_a?(Class) and type != nil
+      classflag = true
+      firstclass = self[0].class.ancestors[0]
+      self.my_each do |m|
+        if m.class.ancestors[0] != firstclass
+          classflag = false
+        end
+      end
+      classflag
+    end
+  end
 end
+
 
 # [1,2,3,4,5].my_each {|n| print n}
 # p [1,2,3,4,5].my_select { |n| n.even? }
+# p ["text", "tas", "aasdf"].my_all? { |n| n.length <= 2 }
+# p [nil].my_all?(Numeric) { |n| n.length >= 3 }
+
+# p [2, 1.5, 4i].my_all?(Numeric)
+# p 3.14.class.name
